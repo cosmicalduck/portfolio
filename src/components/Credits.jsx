f6097224cdd723e83932d2ed162bf9cfc8d3d9d6
cosmicalduck <43730688+cosmicalduck@users.noreply.github.com>
2023-04-26 20:19:07 -0300

    modify credits

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -9,7 +9,7 @@ function Credits() {
       <Container>
         <center>
           <p>Banner background from <a href="https://www.freepik.es/vector-gratis/fondo-galaxia-rosa-azul-acuarela_21935015.htm">Freepik</a></p>
-          <p><a href="https://www.freepik.es/vector-gratis/lindo-astronauta-tocando-estrella-ilustracion-icono-vector-dibujos-animados-espacio-ciencia-tecnologia-aislado_29314439.htm#page=2&query=astronaut&position=33&from_view=search&track=robertav1_2_sidr">Banner image from catalyststuff</a> in Freepik</p>
+          <p><a href="https://www.freepik.es/vector-gratis/lindo-astronauta-tocando-estrella-ilustracion-icono-vector-dibujos-animados-espacio-ciencia-tecnologia-aislado_29314439.htm#page=2&query=astronaut&position=33&from_view=search&track=robertav1_2_sidr">Banner moving image from catalyststuff</a> in Freepik</p>
         </center>
       </Container>
     </footer>
